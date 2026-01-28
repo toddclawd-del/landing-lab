bf2818b8d4866e2c37d4c20f3a133a0adb180c9c
toddclawd-del <toddclawd@gmail.com>
2026-01-28 08:20:36 -0700

    feat: add Voodoo Ranger-inspired bracket POC
    Proof of concept for March Madness bracket with:
    - Dark Voodoo Ranger aesthetic (black/lime/orange/purple)
    - Interactive 8-team bracket
    - Click to pick winners, advances through rounds
    - Neon glow effects on selections
    - Champion display when bracket complete
    - Mobile responsive
    
    Route: /voodoo-bracket (also set as default /)

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,19 @@ import AuroraMeshLanding from './pages/2026-01-27-aurora-mesh'
 import VercelMinimal from './pages/2026-01-27-vercel-minimal'
 import KineticTypography from './pages/2026-01-28-kinetic-typography'
 import SunnySide from './pages/2026-01-28-sunny-side'
+import VoodooBracket from './pages/2026-01-28-voodoo-bracket'
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<SunnySide />} />
+        <Route path="/" element={<VoodooBracket />} />
         <Route path="/home" element={<Home />} />
         <Route path="/aurora-mesh" element={<AuroraMeshLanding />} />
         <Route path="/vercel-minimal" element={<VercelMinimal />} />
         <Route path="/kinetic-typography" element={<KineticTypography />} />
         <Route path="/sunny-side" element={<SunnySide />} />
+        <Route path="/voodoo-bracket" element={<VoodooBracket />} />
       </Routes>
     </HashRouter>
   )
